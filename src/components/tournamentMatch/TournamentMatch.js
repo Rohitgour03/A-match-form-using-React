55b9997269cb771e285081392ae3ca2857685e54
Rohit Gour <rohitgourrg999@gmail.com>
2022-06-14 16:47:51 +0530

    Applied the date picker on tournament match

--- src/components/tournamentMatch/TournamentMatch.js
+++ src/components/tournamentMatch/TournamentMatch.js
@@ -1,4 +1,6 @@
 import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function TournamentMatch(props) {
 
@@ -17,20 +19,48 @@ function TournamentMatch(props) {
     <div className="tournamentMatchCtn">
       <h2>Tournament Match Form</h2>
       <div className="inputs-ctn">
-        <input
+      <DatePicker
+          selected={props.matchData.startDate}
+          dateFormat="dd/MM/yyyy"
+          placeholderText="Start Date"
+          onChange={(date) => {
+            props.setMatchData((prevData) => {
+              return {
+                ...prevData,
+                startDate: date,
+              };
+            });
+            console.log(date);
+          }}
+        />
+        {/* <input
           type="datetime-local"
           name="startDate"
           id="startDate"
           onChange={handleChange}
           value={props.matchData.startDate}
+        /> */}
+        <DatePicker
+          selected={props.matchData.endDate}
+          dateFormat="dd/MM/yyyy"
+          placeholderText="End Date"
+          onChange={(date) => {
+            props.setMatchData((prevData) => {
+              return {
+                ...prevData,
+                endDate: date,
+              };
+            });
+            console.log(date)
+          }}
         />
-        <input
+        {/* <input
           type="datetime-local"
           name="endDate"
           id="endDate"
           onChange={handleChange}
           value={props.matchData.endDate}
-        />
+        /> */}
         <input
           type="text"
           name="matchLocation"
